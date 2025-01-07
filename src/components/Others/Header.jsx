bd79a8302c18babbe870bc97bf2add7c4c2ca254
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-01-07 17:43:42 +0530

    login Done

---
+++ src/components/Others/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+
+        <div className="flex items-end justify-between">
+            <h1 className='text-white'>Hello <br /> Kaif👋</h1>
+            <button>Log Out</button>
+        </div>
+  )
+}
+
+export default Header
